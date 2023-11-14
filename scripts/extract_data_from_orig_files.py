6076ab0ddc5874f445d92bfcbfd8fa3658113c12
Jeffrey <jeffrey@visual-trials.io>
2023-11-14 11:04:15 +0100

    extracting polygon data + cleanup

--- scripts/extract_data_from_orig_files.py
+++ scripts/extract_data_from_orig_files.py
@@ -9,8 +9,6 @@ def parse_object_file(u2_bin, file_name):
 
     u2_object = {
         'file_name' : file_name,
-        'nr_of_vertices' : 0,
-        'vertices' : [],
     }
 
     pos = 0
@@ -21,57 +19,115 @@ def parse_object_file(u2_bin, file_name):
         nr_of_bytes = int.from_bytes(u2_bin[pos0+4:pos0+8], byteorder='little')
         
         if (u2_bin[pos0:pos0+4] == b'VERS'):
-            # We dont really care about the version, so we dont keep it
             version = int.from_bytes(u2_bin[pos:pos+2], byteorder='little')
             print('VERS: ' + str(version/256))
+            # We dont really care about the version, so we dont keep it
         
         elif (u2_bin[pos0:pos0+4] == b'NAME'):
-            # FIXME: we need to check for NUL termination!
-            name = u2_bin[pos:pos+nr_of_bytes].decode("utf-8")
+            # Note: a little dirty way to deal with NUL termination here...
+            name = u2_bin[pos:pos+nr_of_bytes].decode("utf-8").split('\0')[0]
             print('NAME: ' + name)
             u2_object['name'] = name
         elif (u2_bin[pos0:pos0+4] == b'VERT'):
             nr_of_vertices = int.from_bytes(u2_bin[pos:pos+2], byteorder='little')
+            pos += 2
+            # Dummy word
+            pos += 2
             
-            pos += 4
             vertices = []
             for vert_index in range(nr_of_vertices):
                 x = int.from_bytes(u2_bin[pos:pos+4], byteorder='little', signed=True)/10
-                y = int.from_bytes(u2_bin[pos+4:pos+8], byteorder='little', signed=True)/10
-                z = int.from_bytes(u2_bin[pos+8:pos+12], byteorder='little', signed=True)/10
-                normal = int.from_bytes(u2_bin[pos+12:pos+14], byteorder='little', signed=True)
+                pos += 4
+                y = int.from_bytes(u2_bin[pos:pos+4], byteorder='little', signed=True)/10
+                pos += 4
+                z = int.from_bytes(u2_bin[pos:pos+4], byteorder='little', signed=True)/10
+                pos += 4
+                normal_index = int.from_bytes(u2_bin[pos:pos+2], byteorder='little', signed=True)
+                pos += 2
                 # RESERVED not retrieved
+                pos += 2
                 
-                vertex = { 'x' : x, 'y': y, 'z': z, 'normal': normal }
+                vertex = { 'x' : x, 'y': y, 'z': z, 'normal_index': normal_index }
                 vertices.append(vertex)
-                
-                pos += 16 # x:4, y:4, z:4, normal:2, RESERVED:2
             
             print('VERT: ' + str(nr_of_vertices))
             print(vertices)
-            
+            u2_object['vertices'] = vertices
             
         elif (u2_bin[pos0:pos0+4] == b'NORM'):
             nr_of_normals = int.from_bytes(u2_bin[pos:pos+2], byteorder='little')
+            pos += 2
+            # FIXME: nnum1 is NOT extracted here!
+            pos += 2
             
-            pos += 4
             normals = []
             for norm_index in range(nr_of_normals):
                 x = int.from_bytes(u2_bin[pos:pos+2], byteorder='little', signed=True)/16384
-                y = int.from_bytes(u2_bin[pos+2:pos+4], byteorder='little', signed=True)/16384
-                z = int.from_bytes(u2_bin[pos+4:pos+6], byteorder='little', signed=True)/16384
+                pos += 2
+                y = int.from_bytes(u2_bin[pos:pos+2], byteorder='little', signed=True)/16384
+                pos += 2
+                z = int.from_bytes(u2_bin[pos:pos+2], byteorder='little', signed=True)/16384
+                pos += 2
                 # RESERVED not retrieved
+                pos += 2
                 
                 normal = { 'x' : x, 'y': y, 'z': z }
                 normals.append(normal)
-                
-                pos += 8 # x:2, y:2, z:2, RESERVED:2
             
             print('NORM: ' + str(nr_of_normals))
             print(normals)
+            u2_object['normals'] = normals
             
         elif (u2_bin[pos0:pos0+4] == b'POLY'):
-            print('POLY')
+        
+            # Zero word
+            pos += 2
+            
+            polygon_index = 0
+            polygon_data_by_pos = {}
+            
+            end_of_section = pos + nr_of_bytes
+            # FIXME: arbritary -4 here, to prevent going too far...
+            while(pos < end_of_section-4):
+                # FIXME: remember the POSITION this polygon START!
+                start_polygon_data_pos = pos
+                
+                sides = int.from_bytes(u2_bin[pos:pos+1], byteorder='little' )
+                pos += 1
+                # FIXME: we probably want to extract the individual bits from this flag-byte!
+                flags = int.from_bytes(u2_bin[pos:pos+1], byteorder='little' )
+                pos += 1
+                color_index = int.from_bytes(u2_bin[pos:pos+1], byteorder='little' )
+                pos += 1
+                # RESERVED
+                pos += 1
+
+                normal_index = int.from_bytes(u2_bin[pos:pos+2], byteorder='little', signed=True)
+                pos += 2
+                
+                vertex_indices = []
+                for side_index in range(sides):
+                    vertex_index = int.from_bytes(u2_bin[pos:pos+2], byteorder='little', signed=True)
+                    pos += 2
+                    vertex_indices.append(vertex_index)
+                
+                polygon_data = {
+                    'sides' : sides,
+                    'flags' : flags,
+                    'color_index' : color_index,
+                    'normal_index' : normal_index,
+                    'vertex_indices' : vertex_indices
+                }
+                
+                print('POLY: s:' + str(sides) + ' f:' + str(flags) + ' c:' + str(color_index)+ ' n:' + str(normal_index) + ' v:' + str(vertex_indices))
+                #print(polygon_data)
+                polygon_data_by_pos[start_polygon_data_pos] = polygon_data
+                polygon_index += 1
+                
+            # TODO: do we really want to add this polygon datas to the object here?
+            u2_object['polygon_data_by_pos'] = polygon_data_by_pos
+            
+            
         elif (u2_bin[pos0:pos0+3] == b'ORD'):
             print('ORD')
             
