60e4b04459f0235a9efb18ec3f36565e69b00630
mooinglemur <mooinglemur@users.noreply.github.com>
2023-12-12 19:13:39 -0700

    initial import of Forest POC

--- src/util_palette.s
+++ src/util_palette.s
@@ -1,11 +1,27 @@
 ; functions
 .export setup_palette_fade
+.export setup_palette_fade2
+.export setup_palette_fade3
+.export setup_palette_fade4
+
 .export apply_palette_fade_step
+.export apply_palette_fade_step2
+.export apply_palette_fade_step3
+.export apply_palette_fade_step4
+
 .export flush_palette
+.export flush_palette2
+.export flush_palette3
+.export flush_palette4
+
+; only one of these
 .export cycle_palette
 
 ; variables
 .export target_palette
+.export target_palette2
+.export target_palette3
+.export target_palette4
 
 .include "x16.inc"
 
@@ -17,17 +33,47 @@
 
 target_palette:
 	.res 128
+target_palette2:
+	.res 128
+target_palette3:
+	.res 128
+target_palette4:
+	.res 128
 
 new_palette_exp:
+	.res 256
+new_palette_exp2:
+	.res 256
+new_palette_exp3:
+	.res 256
+new_palette_exp4:
 	.res 256
 
 diff_palette_exp:
 	.res 256
+diff_palette_exp2:
+	.res 256
+diff_palette_exp3:
+	.res 256
+diff_palette_exp4:
+	.res 256
 
 fade_iter:
 	.res 1
+fade_iter2:
+	.res 1
+fade_iter3:
+	.res 1
+fade_iter4:
+	.res 1
 
 palette_offset:
+	.res 1
+palette_offset2:
+	.res 1
+palette_offset3:
+	.res 1
+palette_offset4:
 	.res 1
 
 ; pass palette offset in A
@@ -92,6 +138,196 @@ pal44:
 
 	rts
 .endproc
+
+; pass palette offset in A
+; after populating target_palette2
+.proc setup_palette_fade2
+	sta palette_offset2
+	jsr point_data0_to_palette
+
+	; split xRGB values out of the first 64 existing palette entries
+	ldx #0
+palexpand:
+	lda Vera::Reg::Data0
+	pha
+	and #$0f
+	sta new_palette_exp2,x
+	inx
+	pla
+	lsr
+	lsr
+	lsr
+	lsr
+	sta new_palette_exp2,x
+	inx
+	bne palexpand
+
+	; diff between the existing palette and new one
+	ldx #0
+	ldy #0
+paldiff:
+	lda target_palette2,x
+	and #$0f
+	sec
+	sbc new_palette_exp2,y
+	sta diff_palette_exp2,y
+	iny
+	lda target_palette2,x
+	lsr
+	lsr
+	lsr
+	lsr
+	sec
+	sbc new_palette_exp2,y
+	sta diff_palette_exp2,y
+	inx
+	iny
+	bne paldiff
+
+	; turn new_palette_exp into 4.4 fixed point
+	ldy #0
+pal44:
+	lda new_palette_exp2,y
+	asl
+	asl
+	asl
+	asl
+	sta new_palette_exp2,y
+	iny
+	bne pal44
+
+	lda #16
+	sta fade_iter2
+
+	rts
+.endproc
+
+; pass palette offset in A
+; after populating target_palette3
+.proc setup_palette_fade3
+	sta palette_offset3
+	jsr point_data0_to_palette
+
+	; split xRGB values out of the first 64 existing palette entries
+	ldx #0
+palexpand:
+	lda Vera::Reg::Data0
+	pha
+	and #$0f
+	sta new_palette_exp3,x
+	inx
+	pla
+	lsr
+	lsr
+	lsr
+	lsr
+	sta new_palette_exp3,x
+	inx
+	bne palexpand
+
+	; diff between the existing palette and new one
+	ldx #0
+	ldy #0
+paldiff:
+	lda target_palette3,x
+	and #$0f
+	sec
+	sbc new_palette_exp3,y
+	sta diff_palette_exp3,y
+	iny
+	lda target_palette3,x
+	lsr
+	lsr
+	lsr
+	lsr
+	sec
+	sbc new_palette_exp3,y
+	sta diff_palette_exp3,y
+	inx
+	iny
+	bne paldiff
+
+	; turn new_palette_exp into 4.4 fixed point
+	ldy #0
+pal44:
+	lda new_palette_exp3,y
+	asl
+	asl
+	asl
+	asl
+	sta new_palette_exp3,y
+	iny
+	bne pal44
+
+	lda #16
+	sta fade_iter3
+
+	rts
+.endproc
+
+; pass palette offset in A
+; after populating target_palette4
+.proc setup_palette_fade4
+	sta palette_offset4
+	jsr point_data0_to_palette
+
+	; split xRGB values out of the first 64 existing palette entries
+	ldx #0
+palexpand:
+	lda Vera::Reg::Data0
+	pha
+	and #$0f
+	sta new_palette_exp4,x
+	inx
+	pla
+	lsr
+	lsr
+	lsr
+	lsr
+	sta new_palette_exp4,x
+	inx
+	bne palexpand
+
+	; diff between the existing palette and new one
+	ldx #0
+	ldy #0
+paldiff:
+	lda target_palette4,x
+	and #$0f
+	sec
+	sbc new_palette_exp4,y
+	sta diff_palette_exp4,y
+	iny
+	lda target_palette4,x
+	lsr
+	lsr
+	lsr
+	lsr
+	sec
+	sbc new_palette_exp4,y
+	sta diff_palette_exp4,y
+	inx
+	iny
+	bne paldiff
+
+	; turn new_palette_exp into 4.4 fixed point
+	ldy #0
+pal44:
+	lda new_palette_exp4,y
+	asl
+	asl
+	asl
+	asl
+	sta new_palette_exp4,y
+	iny
+	bne pal44
+
+	lda #16
+	sta fade_iter4
+
+	rts
+.endproc
+
 
 .proc cycle_palette ; A = offset (index), X = length (index)
 	sta offset
@@ -232,7 +468,109 @@ end:
 	rts
 .endproc
 
+.proc apply_palette_fade_step2
+	lda fade_iter2
+	beq end
+
+	dec fade_iter2
+
+	ldx #0
+	ldy #0
+palloop:
+	lda new_palette_exp2,y
+	clc
+	adc diff_palette_exp2,y
+	sta new_palette_exp2,y
+	lsr
+	lsr
+	lsr
+	lsr
+	sta target_palette2,x
+	iny
+	lda new_palette_exp2,y
+	clc
+	adc diff_palette_exp2,y
+	sta new_palette_exp2,y
+	and #$f0
+	ora target_palette2,x
+	sta target_palette2,x
+	inx
+	iny
+	bne palloop
+end:
+	rts
+.endproc
+
+.proc apply_palette_fade_step3
+	lda fade_iter3
+	beq end
+
+	dec fade_iter3
+
+	ldx #0
+	ldy #0
+palloop:
+	lda new_palette_exp3,y
+	clc
+	adc diff_palette_exp3,y
+	sta new_palette_exp3,y
+	lsr
+	lsr
+	lsr
+	lsr
+	sta target_palette3,x
+	iny
+	lda new_palette_exp3,y
+	clc
+	adc diff_palette_exp3,y
+	sta new_palette_exp3,y
+	and #$f0
+	ora target_palette3,x
+	sta target_palette3,x
+	inx
+	iny
+	bne palloop
+end:
+	rts
+.endproc
+
+
+.proc apply_palette_fade_step4
+	lda fade_iter4
+	beq end
+
+	dec fade_iter4
+
+	ldx #0
+	ldy #0
+palloop:
+	lda new_palette_exp4,y
+	clc
+	adc diff_palette_exp4,y
+	sta new_palette_exp4,y
+	lsr
+	lsr
+	lsr
+	lsr
+	sta target_palette4,x
+	iny
+	lda new_palette_exp4,y
+	clc
+	adc diff_palette_exp4,y
+	sta new_palette_exp4,y
+	and #$f0
+	ora target_palette4,x
+	sta target_palette4,x
+	inx
+	iny
+	bne palloop
+end:
+	rts
+.endproc
+
+
 .proc flush_palette
+	lda palette_offset
 	jsr point_data0_to_palette
 
 	; write palette shadow to VERA
@@ -244,8 +582,47 @@ shadowpal:
 	bne shadowpal
 .endproc
 
+.proc flush_palette2
+	lda palette_offset2
+	jsr point_data0_to_palette
+
+	; write palette shadow to VERA
+	ldx #128
+shadowpal:
+	lda target_palette2-128,x
+	sta Vera::Reg::Data0
+	inx
+	bne shadowpal
+.endproc
+
+.proc flush_palette3
+	lda palette_offset3
+	jsr point_data0_to_palette
+
+	; write palette shadow to VERA
+	ldx #128
+shadowpal:
+	lda target_palette3-128,x
+	sta Vera::Reg::Data0
+	inx
+	bne shadowpal
+.endproc
+
+.proc flush_palette4
+	lda palette_offset4
+	jsr point_data0_to_palette
+
+	; write palette shadow to VERA
+	ldx #128
+shadowpal:
+	lda target_palette4-128,x
+	sta Vera::Reg::Data0
+	inx
+	bne shadowpal
+.endproc
+
+
 .proc point_data0_to_palette
-	lda palette_offset
 	asl
 	php
 	clc
