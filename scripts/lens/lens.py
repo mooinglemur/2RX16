8b78d5b8d9529a570cd917822bc02bf78f43ce29
Jeffrey <jeffrey@visual-trials.io>
2023-12-15 12:00:12 +0100

    using lens image as shape, 3 shades of blue

--- scripts/lens/lens.py
+++ scripts/lens/lens.py
@@ -6,7 +6,7 @@ import time
 import random
 
 DO_MOVE_LENS = True
-DRAW_NEW_PALETTE = True
+DRAW_NEW_PALETTE = False
 
 # TODO: we should *ALSO* use the LENS.png image! (with the 'glaring' of the lens)
 
@@ -27,7 +27,11 @@ screen_width = 320
 screen_height = 200
 
 BITMAP_QUADRANT_BUFFER = 0x6000
+# FIXME: we need lens_width and lens_height INSTEAD!
 lens_size = 100
+half_lens_width = 59 # 117 total width, 1 pixel overlapping so 117 // 2 + 1 = 59 (or 118 // 2 if you will)
+half_lens_height = 52 # 103 total height, 1 pixel overlapping so 103 // 2 + 1 = 52 (or 104 // 2 if you will)
+# FIXME: what do we do with lens_radius? Remove it?
 lens_radius = lens_size/2
 lens_zoom = 16
 scale = 2
@@ -42,25 +46,9 @@ im_lens = Image.open(lens_image_filename)
 lens_px = im_lens.load()
 lens_palette_bytes = im_lens.getpalette()
 
-print(lens_palette_bytes)
+#print(lens_palette_bytes)
 #print(lens_px)
 
-lens_colors = {}
-for lens_source_y in range(lens_source_image_height):
-
-    for lens_source_x in range(lens_source_image_width):
-
-        lens_pixel_color_index = px[lens_source_x, lens_source_y]
-        
-        if (lens_pixel_color_index not in lens_colors):
-            lens_colors[lens_pixel_color_index] = 0
-            
-        lens_colors[lens_pixel_color_index] += 1
-
-
-#print(lens_colors)
-#exit()
-'''
 # We first convert to 12-bit COLORS
 colors_12bit = []
 
@@ -80,29 +68,6 @@ while (byte_index < nr_of_palette_bytes):
     blue = blue & 0xF0
     
     colors_12bit.append((red, green, blue))
-'''
-
-
-# We first convert to 12-bit COLORS
-colors_12bit = []
-
-byte_index = 0
-nr_of_palette_bytes = 3*256
-while (byte_index < nr_of_palette_bytes):
-    
-    red = palette_bytes[byte_index]
-    byte_index += 1
-    green = palette_bytes[byte_index]
-    byte_index += 1
-    blue = palette_bytes[byte_index]
-    byte_index += 1
-    
-    red = red & 0xF0
-    green = green & 0xF0
-    blue = blue & 0xF0
-    
-    colors_12bit.append((red, green, blue))
-
 
 offset_blue_colors = 64
 for clr_idx in range(0, offset_blue_colors):
@@ -111,13 +76,30 @@ for clr_idx in range(0, offset_blue_colors):
     red = new_color[0]
     green = new_color[1]
     blue = new_color[2]
+
+    red_dark = red // 2
+    green_dark = green // 2
+
+    red_light = 256 - (256 - red) // 2
+    green_light = 256 - (256 - green) // 2
     
-    new_blue = 256 - (256 - blue) // 2
+    new_blue_dark = 256 - int((256 - blue) / 1.2)
+    new_blue_normal = 256 - int((256 - blue) / 1.5)
+    new_blue_light = 256 - int((256 - blue) / 2.0)
     
-    # colors 64 through 127 are going to be blue-ish
-    blue_ish_color = (red, green, new_blue)
-    colors_12bit[clr_idx+offset_blue_colors] = blue_ish_color
+    # colors 64 through 127 are going to be dark blue-ish
+    blue_ish_color_dark = (red_dark, green_dark, new_blue_dark)
+    colors_12bit[clr_idx+offset_blue_colors*1] = blue_ish_color_dark
     
+    # colors 128 through 191 are going to be normal blue-ish
+    blue_ish_color_normal = (red_dark, green_dark, new_blue_normal)
+    colors_12bit[clr_idx+offset_blue_colors*2] = blue_ish_color_normal
+    
+    # colors 192 through 255 are going to be light blue-ish
+    blue_ish_color_light = (red_dark, green_dark, new_blue_light)
+    colors_12bit[clr_idx+offset_blue_colors*3] = blue_ish_color_light
+    
+
 # Printing out asm for palette:
 palette_string = ""
 for new_color in colors_12bit:
@@ -140,21 +122,45 @@ background_color = (0,0,0)
 # x,y offsets
 lens_offsets = []
 
+# black/transparent + 3 shades of blue for each pixel in the lens
+lens_pixels = []
 
-def init_lens():
+def init_lens(lens_px):
 
     d = lens_zoom
     d2 = lens_zoom*lens_zoom
     r2 = lens_radius*lens_radius
     
-    hlf = int(lens_radius)
+    hlfh = half_lens_height
+    hlfw = half_lens_width
     
-    full = lens_radius*lens_radius + lens_radius*lens_radius
+# FIXME! use half width and half height
+    full = hlfw*hlfw + hlfh*hlfh
 
-    for y in range(int(lens_size)):
+    for y in range(int((half_lens_height * 2) - 1)):
         lens_offsets.append([])
-        for x in range(int(lens_size)):
+        lens_pixels.append([])
+        for x in range(int((half_lens_width * 2) - 1)):
             lens_offsets[y].append(None)
+            lens_pixels[y].append(None)
+            
+    # We use the lens image to fill lens_pixels with the values 0 (black/transparent), 1, 2 and 3 (shades of blue)
+
+    lens_source_image_start_x = 19
+    lens_source_image_start_y = 8
+    
+    for lens_source_y in range(lens_source_image_start_y, lens_source_image_start_y + (half_lens_height * 2) - 1):
+        for lens_source_x in range(lens_source_image_start_x, lens_source_image_start_x + (half_lens_width * 2) - 1):
+            lens_pixel_color_index = lens_px[lens_source_x, lens_source_y]
+            
+            # Map from colors 0-4 to 0-3 
+            if (lens_pixel_color_index == 1):
+                lens_pixel_color_index = 0
+            elif (lens_pixel_color_index > 0):
+                lens_pixel_color_index -= 1
+                
+            lens_pixels[lens_source_y - lens_source_image_start_y][lens_source_x - lens_source_image_start_x] = lens_pixel_color_index
+            
 
     # 2R analysis: https://fabiensanglard.net/second_reality/
             
@@ -174,67 +180,41 @@ def init_lens():
     #     *py=(int)(((double)y+fy)*new);
     # }
             
-    for y in range(hlf):
-        y2 = y*y
-        for x in range(hlf):
+    for y in range(half_lens_height):
+# FIXME: we need to adjust y by 9/8?
+        y2 = (y*9/8)*(y*9/8)
+        for x in range(half_lens_width):
             x2 = x*x
-            if ((x2 + y2) <= r2):
-                # old way:
-                # shift = d / math.sqrt(d2 - (x2 + y2 - r2))
-                # x_shift = shift * x - x
-                # y_shift = shift * y - y
+# FIXME: check if lens color is black or not!
+            #if ((x2 + y2) <= r2):
+            if (lens_pixels[hlfh-1+y][hlfw-1+x] != 0):
+            
+                now = full - (x2 + y2)
+                if now < 1:
+                    now = 1
+# FIXME: do we need to adjust this too?
+                new = 250 / (now**0.69)
+                fx = (random.random() - 0.5) * 1.2
+                fy = (random.random() - 0.5) * 1.2
+                px = int((x+fx)*new)
+                py = int((y+fy)*new)
                 
-                '''
-                if (False):
-                    distance_from_center = math.sqrt(x2 + y2) / lens_radius # distance from center: 0.0 -> 1.0
-                    
-                    # We now use the quadratic function to create the lens effect
-                    #distance_from_center *= 0.9
-                    #distance_from_center += 0.1
-                    
-                    # FIXME: this is ALMOST correct!
-                    desired_distance_from_center = distance_from_center * distance_from_center
-                    
-                    if distance_from_center > 0:
-                        ratio = desired_distance_from_center / distance_from_center
-                    else:
-                        ratio = 2
-                        
-                    if ratio < 0:
-                        ratio = 0
-                    
-                    # FIXME: should we round UP or DOWN here?
-                    #x_shift = int(ratio * x - x) 
-                    #y_shift = int(ratio * y - y) 
-                    x_shift = int(ratio * x - x + random.random()*1.5 - 0.75) 
-                    y_shift = int(ratio * y - y + random.random()*1.5 - 0.75)
-                '''
+                x_shift = px - x
+                y_shift = py - y
                 
-                if (True):
-                    now = full - (x2 + y2)
-                    if now < 1:
-                        now = 1
-                    new = 220 / (now**0.69)
-                    fx = (random.random() - 0.5) * 1.2
-                    fy = (random.random() - 0.5) * 1.2
-                    px = int((x+fx)*new)
-                    py = int((y+fy)*new)
-                    
-                    x_shift = px - x
-                    y_shift = py - y
-                    
-                
+# FIXME: this now OVERLAPS! CHECK IT!
                 # Inside the lens the pixel gets shifted according to the quadrant it is in
-                lens_offsets[ hlf   + y][ hlf   + x] = (  x_shift,  y_shift)
-                lens_offsets[ hlf-1 - y][ hlf   + x] = (  x_shift, -y_shift)
-                lens_offsets[ hlf   + y][ hlf-1 - x] = ( -x_shift,  y_shift)
-                lens_offsets[ hlf-1 - y][ hlf-1 - x] = ( -x_shift, -y_shift)
+                lens_offsets[ hlfh-1 + y][ hlfw-1 + x] = (  x_shift,  y_shift)
+                lens_offsets[ hlfh-1 - y][ hlfw-1 + x] = (  x_shift, -y_shift)
+                lens_offsets[ hlfh-1 + y][ hlfw-1 - x] = ( -x_shift,  y_shift)
+                lens_offsets[ hlfh-1 - y][ hlfw-1 - x] = ( -x_shift, -y_shift)
             else:
+# FIXME: this now OVERLAPS! CHECK IT!
                 # Outside the lens there is no distortion/shift
-                lens_offsets[ hlf   + y][ hlf   + x] = (None,None)
-                lens_offsets[ hlf-1 - y][ hlf   + x] = (None,None)
-                lens_offsets[ hlf   + y][ hlf-1 - x] = (None,None)
-                lens_offsets[ hlf-1 - y][ hlf-1 - x] = (None,None)
+                lens_offsets[ hlfh-1 + y][ hlfw-1 + x] = (None,None)
+                lens_offsets[ hlfh-1 - y][ hlfw-1 + x] = (None,None)
+                lens_offsets[ hlfh-1 + y][ hlfw-1 - x] = (None,None)
+                lens_offsets[ hlfh-1 - y][ hlfw-1 - x] = (None,None)
 
 # Generate 'download' code (to copy quarter of a cirle from VRAM to Fixed RAM)
 #
@@ -250,6 +230,11 @@ def init_lens():
 #  - Free memory at address BITMAP_QUADRANT_BUFFER (lens_radius*lens_radius in size)
 #
 def generate_download_and_upload_code():
+
+# FIXME! we need 4 * 2 upload code parts!!
+# FIXME! we need 4 * 2 upload code parts!!
+# FIXME! we need 4 * 2 upload code parts!!
+
     download1_code = []
     download2_code = []
     upload1_code = []
@@ -260,6 +245,9 @@ def generate_download_and_upload_code():
     
     hlf = int(lens_radius)
 
+# FIXME: we download and upload the PIXELS on the AXIS **TWICE**!
+# FIXME: we download and upload the PIXELS on the AXIS **TWICE**!
+# FIXME: we download and upload the PIXELS on the AXIS **TWICE**!
     for y in range(hlf):
         for x in range(hlf):
             (x_shift, y_shift) = lens_offsets[ hlf   + y][ hlf   + x]
@@ -367,8 +355,12 @@ pygame.display.set_caption('X16 2R Lens test')
 screen = pygame.display.set_mode((screen_width*2, screen_height*2))
 clock = pygame.time.Clock()
 
-init_lens()
+init_lens(lens_px)
 
+# FIXME! Enable this again!
+# FIXME! Enable this again!
+# FIXME! Enable this again!
+'''
 (download1_code, download2_code, upload1_code, upload2_code) = generate_download_and_upload_code()
 
 tableFile = open(download1_code_filename, "wb")
@@ -390,6 +382,7 @@ tableFile = open(upload2_code_filename, "wb")
 tableFile.write(bytearray(upload2_code))
 tableFile.close()
 print("upload code 2 written to file: " + upload2_code_filename)
+'''
 
 bitmap_data = []
 # FIXME: we now use 0 as BLACK, but in the bitmap a DIFFERENT color index is used as BLACK!
@@ -480,22 +473,25 @@ def run():
                 pygame.draw.rect(screen, pixel_color, pygame.Rect(x_screen*scale, y_screen*scale, scale, scale))
 
                 
-        for lens_y in range(int(lens_size)):
-            for lens_x in range(int(lens_size)):
+        for lens_y in range(int(half_lens_height*2-1)):
+            for lens_x in range(int(half_lens_width*2-1)):
+                
+                # If the lens_pixel color is black/transparent, we dont do anything
+                if (lens_pixels[lens_y][lens_x] == 0):
+                    continue
+                
+                blue_shift = lens_pixels[lens_y][lens_x]
                 
                 (x_shift, y_shift) = lens_offsets[lens_y][lens_x]
                 
-                if (x_shift is None):
-                    continue
+                source_y = lens_pos_y - half_lens_height + lens_y + y_shift
+                source_x = lens_pos_x - half_lens_width + lens_x + x_shift
                 
-                source_y = lens_pos_y - lens_radius + lens_y + y_shift
-                source_x = lens_pos_x - lens_radius + lens_x + x_shift
-                
-                if (source_y < source_image_height):
-                    pixel_color = colors_12bit[px[source_x, source_y] + offset_blue_colors]
+                if (source_y >= 0 and source_y < source_image_height and source_x >= 0 and source_x < source_image_width):
+                    pixel_color = colors_12bit[px[source_x, source_y] + offset_blue_colors*blue_shift]
                     
-                    y_screen = lens_pos_y - lens_radius + lens_y
-                    x_screen = lens_pos_x - lens_radius + lens_x
+                    y_screen = lens_pos_y - half_lens_height + lens_y
+                    x_screen = lens_pos_x - half_lens_width + lens_x
                     
                     pygame.draw.rect(screen, pixel_color, pygame.Rect(x_screen*scale, y_screen*scale, scale, scale))
                 else:
