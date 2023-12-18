5a1d6934e8afbf01f0e9466e493d622e6c409c99
Jeffrey <jeffrey@visual-trials.io>
2023-12-18 16:31:05 +0100

    starting to animate: testing positions and rotation alignments

--- scripts/lens/rotazoom.py
+++ scripts/lens/rotazoom.py
@@ -185,8 +185,19 @@ def generate_pos_and_rotation_frames():
     scalea = -0.01
     frame = 0
     
+    
+    cosine_rotate_low  = "cosine_rotate_low:\n  .byte "
+    cosine_rotate_high = "cosine_rotate_high:\n  .byte "
+    sine_rotate_low  = "sine_rotate_low:\n  .byte "
+    sine_rotate_high = "sine_rotate_high:\n  .byte "
+    x_position_low  = "x_position_low:\n  .byte "
+    x_position_high = "x_position_high:\n  .byte "
+    y_position_low  = "y_position_low:\n  .byte "
+    y_position_high = "y_position_high:\n  .byte "
+    
+    first_number_added = False
 # FIXME!        for frame_nr in range(2000):
-    for frame in range(100):
+    for frame in range(256):
     
         x = 70.0 * math.sin(d1) - 30
         y = 70.0 * math.cos(d1) + 60
@@ -202,7 +213,33 @@ def generate_pos_and_rotation_frames():
         #putw(xa,fp);
         #putw(ya,fp);
         #rotate(x,y,xa,ya);
-        print('x: '+str(x)+' y: '+str(y)+' xa: '+str(xa)+' ya: '+str(ya))
+        
+        # IMPORTANT: y has to be NEGATED!
+        
+        print('frame: '+str(frame)+' x: '+str(x)+' y: '+str(y)+' xa: '+str(xa)+' ya: '+str(ya)+' sc: '+str(scale))
+        print(str(int(ya / 8) % 256))
+        
+        comma = ", "
+        if (not first_number_added):
+            comma = ""
+            first_number_added = True
+        
+        # == cosine_rotate is +ya / 8 ==
+        # == sine_rotate is   -xa / 8 ==
+        # == x_position is     -x / 2 ==
+        # == y_position is     +y / 2 ==
+        
+        cosine_rotate_low += comma + str(int(ya / 8) % 256)
+        cosine_rotate_high += comma + str(int(ya / 8) // 256)
+        
+        sine_rotate_low += comma + str(int(-xa / 8) % 256)
+        sine_rotate_high += comma + str(int(-xa / 8) // 256)
+        
+        x_position_low += comma + str(int(-x / 2) % 256)
+        x_position_high += comma + str(int(-x / 2) // 256)
+        
+        y_position_low += comma + str(int(y / 2) % 256)
+        y_position_high += comma + str(int(y / 2) // 256)
         
         scale += scalea
 
@@ -226,6 +263,16 @@ def generate_pos_and_rotation_frames():
                 a = 100
             if(scalea < 256):
                 scalea += 0.000001 * a
+
+
+    print(cosine_rotate_low)
+    print(cosine_rotate_high)
+    print(sine_rotate_low)
+    print(sine_rotate_high)
+    print(x_position_low)
+    print(x_position_high)
+    print(y_position_low)
+    print(y_position_high)
 
 
 # FIXME: we need to store the output into a FILE!
