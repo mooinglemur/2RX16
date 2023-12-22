3776a529fdcccef596bbb2a60598939ea9d2d3f7
Jeffrey <jeffrey@visual-trials.io>
2023-12-22 21:13:03 +0100

    showing final result

--- scripts/techno/techno.py
+++ scripts/techno/techno.py
@@ -3,6 +3,9 @@ import pygame
 import math
 import time
 
+SHOW_OFFLINE_BUFFER = False
+SHOW_FRAME_BUFFERS = False
+PRESS_KEY_TO_START = True
 
 screen_width = 320
 screen_height = 200
@@ -22,28 +25,57 @@ frame_buffer_y_margin = 10
 
 pygame.init()
 
+
+clr_blk = (0,0,0) # black
+clr_drk = (221,152,181) # dark
+clr_med = (253,206,238) # medium
+clr_lgt = (253,255,255) # light    # FIXME: not distinguishable from WHITE!
+clr_wht = (253,255,255) # white
+
+palette = [
+    clr_blk,  # 0000b
+    clr_drk,  # 0001b
+    clr_drk,  # 0010b
+    clr_med,  # 0011b
+    
+    clr_drk,  # 0100b
+    clr_med,  # 0101b
+    clr_med,  # 0110b
+    clr_lgt,  # 0111b
+    
+    clr_drk,  # 1000b
+    clr_med,  # 1001b
+    clr_med,  # 1010b
+    clr_lgt,  # 1011b
+    
+    clr_med,  # 1100b
+    clr_lgt,  # 1101b
+    clr_lgt,  # 1110b
+    clr_wht,  # 1111b
+]
+
+
 offline_surface = pygame.Surface((screen_width, screen_height), depth = 8)
 
 frame_buffers = []    
 for frame_buffer_idx in range(8):
-    frame_buffers.append(pygame.Surface((screen_width, screen_height), depth = 8))
-    # frame_buffers.append([0] * screen_width *screen_height)
+    frame_buffer = pygame.Surface((screen_width, screen_height), depth = 8)
+    frame_buffer.set_palette(palette)
+    frame_buffers.append(frame_buffer)
     
 pygame.display.set_caption('X16 2R Techno test')
-screen = pygame.display.set_mode((screen_width*4.5, screen_height*3.5))
+
+screen_size = (screen_width*scale, screen_height*scale)
+final_on_screen_x = 0
+final_on_screen_y = 0
+if SHOW_OFFLINE_BUFFER or SHOW_FRAME_BUFFERS:
+    screen_size = (screen_width*4.5, screen_height*3.5)
+    final_on_screen_x = offline_on_screen_x + screen_width + 10
+    final_on_screen_y = 10 
+
+
+screen = pygame.display.set_mode(screen_size)
 clock = pygame.time.Clock()
-
-#palette = [
-#    (0,0,0),
-#    (255,255,255),
-#]
-#screen.set_palette(palette)
-
-palette = frame_buffers[0].get_palette()
-
-print(palette)
-
-
 
 def combine_offline_with_frame_buffer(offline_surface, frame_buffer, mask):
 
@@ -84,6 +116,9 @@ def run():
     
     
     keep_animating = True
+    if PRESS_KEY_TO_START:
+        keep_animating = False
+    
     
     while running:
         # TODO: We might want to set this to max?
@@ -94,8 +129,7 @@ def run():
             if event.type == pygame.QUIT: 
                 running = False
 
-            '''
-            # if event.type == pygame.KEYDOWN:
+            if event.type == pygame.KEYDOWN:
                     
                 #if event.key == pygame.K_LEFT:
                 #if event.key == pygame.K_RIGHT:
@@ -103,10 +137,13 @@ def run():
                 #if event.key == pygame.K_PERIOD:
                 #if event.key == pygame.K_UP:
                 #if event.key == pygame.K_DOWN:
+                
+                if event.key == pygame.K_SPACE:
+                    # Press SPACE to start animation
+                    keep_animating = True
                     
             #if event.type == pygame.MOUSEMOTION: 
                 # newrect.center = event.pos
-            '''
             
             
             # Original "doit1"
@@ -209,12 +246,20 @@ def run():
             
 
             # Show buffers on screen
-            screen.blit(offline_surface, (offline_on_screen_x, offline_on_screen_y))
+            if SHOW_OFFLINE_BUFFER:
+                screen.blit(offline_surface, (offline_on_screen_x, offline_on_screen_y))
+
+            if SHOW_FRAME_BUFFERS:
+                screen_x = frame_buffers_on_screen_x + int(frame_buffer_idx % 4) * (screen_width+frame_buffer_x_margin)
+                screen_y = frame_buffers_on_screen_y + int(frame_buffer_idx // 4) * (screen_height+frame_buffer_y_margin)
+                screen.blit(frame_buffer, (screen_x, screen_y))
+
             
-            screen_x = frame_buffers_on_screen_x + int(frame_buffer_idx % 4) * (screen_width+frame_buffer_x_margin)
-            screen_y = frame_buffers_on_screen_y + int(frame_buffer_idx // 4) * (screen_height+frame_buffer_y_margin)
-            screen.blit(frame_buffer, (screen_x, screen_y))
+            if (not SHOW_OFFLINE_BUFFER) and (not SHOW_FRAME_BUFFERS):
+                screen.blit(pygame.transform.scale(frame_buffer, (screen_width*scale, screen_height*scale)), (final_on_screen_x, final_on_screen_y))
                 
+            else:
+                screen.blit(frame_buffer, (final_on_screen_x, final_on_screen_y))
         
         
             # in original: "plv++"
