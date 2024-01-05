8701a233bbc8622de9dea592742749f79ad2a80c
Jeffrey <jeffrey@visual-trials.io>
2024-01-05 13:15:16 +0100

    refactoring of how we do clipping

--- scripts/3d_scene/pygame-3d-scene.py
+++ scripts/3d_scene/pygame-3d-scene.py
@@ -452,8 +452,10 @@ def cull_faces_of_objects(view_faces, view_vertices, view_normals):
         
     return (culled_view_faces, culled_view_vertices)
 
-def is_vertex_inside_z_edge(vertex):
+def is_vertex_inside_z_edge(combined_vertices, vertex_index):
 
+    vertex = combined_vertices[vertex_index]
+    
     z = vertex[2]
     
     vertex_is_inside = True
@@ -464,14 +466,20 @@ def is_vertex_inside_z_edge(vertex):
     return vertex_is_inside
     
 
-def clip_vertex_against_z_edge(inside_vertex, outside_vertex):
+def clip_vertex_against_z_edge(combined_vertices, inside_vertex_index, outside_vertex_index):
+
+    inside_vertex = combined_vertices[inside_vertex_index]
+    outside_vertex = combined_vertices[outside_vertex_index]
 
     percentage_to_keep = (inside_vertex[2] - Z_EDGE) / (inside_vertex[2] - outside_vertex[2])
     x_clipped = inside_vertex[0] + (outside_vertex[0] - inside_vertex[0]) * percentage_to_keep
     y_clipped = inside_vertex[1] + (outside_vertex[1] - inside_vertex[1]) * percentage_to_keep
     clipped_vertex = (x_clipped, y_clipped, Z_EDGE)
     
-    return clipped_vertex
+    clipped_vertex_index = len(combined_vertices)
+    combined_vertices.append(clipped_vertex)
+    
+    return clipped_vertex_index
 
 
 def clip_face_against_z_edge(non_clipped_face, combined_vertices):
@@ -483,67 +491,58 @@ def clip_face_against_z_edge(non_clipped_face, combined_vertices):
 
     # We need to check which of these vertices are INSIDE and OUTSIDE of the plane/edge we are clipping against
     
-    inside_vertices = []
-    outside_vertices = []
-    for vertex_index in range(3):
-        non_clipped_vertex = combined_vertices[non_clipped_face['vertex_indices'][vertex_index]]
+    inside_vertex_indices = []
+    outside_vertex_indices = []
+    for vertex_nr in range(3):
+        non_clipped_vertex_index = non_clipped_face['vertex_indices'][vertex_nr]
         
-        vertex_is_inside = is_vertex_inside_z_edge(non_clipped_vertex)
+        vertex_is_inside = is_vertex_inside_z_edge(combined_vertices, non_clipped_vertex_index)
         
         if vertex_is_inside:
             # Since this is an inside vertex, there is no need to clip it, so we just copy it
-            inside_vertices.append(copy.deepcopy(non_clipped_vertex))
+            inside_vertex_indices.append(non_clipped_vertex_index)
         else:
             # We clip the vertex against the edge
-            outside_vertices.append(non_clipped_vertex)
+            outside_vertex_indices.append(non_clipped_vertex_index)
     
-    if (len(inside_vertices) == 0):
+    if (len(inside_vertex_indices) == 0):
         # The triangle is completely outside the edge, we dont add it
         pass
-    elif (len(inside_vertices) == 3):
+    elif (len(inside_vertex_indices) == 3):
         # The triangle is completely inside the edge, we add it as-is
-#        combined_vertices += inside_vertices
         clipped_face = copy.deepcopy(non_clipped_face)
-#        clipped_face['vertex_indices'] = [0+svi,1+svi,2+svi]
         clipped_faces.append(clipped_face)
-    elif (len(inside_vertices) == 1):
+    elif (len(inside_vertex_indices) == 1):
         # Out triangle gets shorter, so we return one smaller triangle
-        combined_vertices.append(inside_vertices[0])
-        clipped_vertex_0 = clip_vertex_against_z_edge(inside_vertices[0], outside_vertices[0])
-        combined_vertices.append(clipped_vertex_0)
-        clipped_vertex_1 = clip_vertex_against_z_edge(inside_vertices[0], outside_vertices[1])
-        combined_vertices.append(clipped_vertex_1)
+        clipped_vertex_index_0 = clip_vertex_against_z_edge(combined_vertices, inside_vertex_indices[0], outside_vertex_indices[0])
+        clipped_vertex_index_1 = clip_vertex_against_z_edge(combined_vertices, inside_vertex_indices[0], outside_vertex_indices[1])
         clipped_face = copy.deepcopy(non_clipped_face)
 # FIXME: we are NOT keeping the CORRECT ORDER of the vertices here!
-        clipped_face['vertex_indices'] = [0+svi,1+svi,2+svi]
+        clipped_face['vertex_indices'] = [inside_vertex_indices[0], clipped_vertex_index_0, clipped_vertex_index_1]
         clipped_face['is_clipped'] = True
         if (DEBUG_CLIP_COLORS):
             clipped_face['color_index'] = 3
         clipped_faces.append(clipped_face)
-    elif (len(inside_vertices) == 2):
+    elif (len(inside_vertex_indices) == 2):
         # We have a quad we have to split into two triangles
         
         # First triangle
-        combined_vertices.append(inside_vertices[0])
-        clipped_vertex_1 = clip_vertex_against_z_edge(inside_vertices[1], outside_vertices[0])
-        combined_vertices.append(clipped_vertex_1)
-        combined_vertices.append(inside_vertices[1])
+        clipped_vertex_index_1 = clip_vertex_against_z_edge(combined_vertices, inside_vertex_indices[1], outside_vertex_indices[0])
         
         clipped_face = copy.deepcopy(non_clipped_face)
 # FIXME: we are NOT keeping the CORRECT ORDER of the vertices here!
-        clipped_face['vertex_indices'] = [0+svi,1+svi,2+svi]
+        clipped_face['vertex_indices'] = [inside_vertex_indices[0], clipped_vertex_index_1, inside_vertex_indices[1]]
         clipped_face['is_clipped'] = True
         if (DEBUG_CLIP_COLORS):
             clipped_face['color_index'] = 2
         clipped_faces.append(clipped_face)
         
         # Second triangle
-        clipped_vertex_0 = clip_vertex_against_z_edge(inside_vertices[0], outside_vertices[0])
-        combined_vertices.append(clipped_vertex_0)
+        clipped_vertex_index_0 = clip_vertex_against_z_edge(combined_vertices, inside_vertex_indices[0], outside_vertex_indices[0])
         
         clipped_face = copy.deepcopy(non_clipped_face)
 # FIXME: we are NOT keeping the CORRECT ORDER of the vertices here!
-        clipped_face['vertex_indices'] = [0+svi,3+svi,1+svi]
+        clipped_face['vertex_indices'] = [inside_vertex_indices[0], clipped_vertex_index_0, clipped_vertex_index_1]
         clipped_face['is_clipped'] = True
         if (DEBUG_CLIP_COLORS):
             clipped_face['color_index'] = 4
