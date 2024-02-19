9f993ab39c70c6ea0485618e730b8b15db88c038
mooinglemur <mooinglemur@users.noreply.github.com>
2024-02-18 23:17:13 -0700

    panic image loads in, does not transition out yet

--- src/moire.s
+++ src/moire.s
@@ -1,10 +1,22 @@
 .import graceful_fail
 
 .import setup_palette_fade
+.import setup_palette_fade2
+.import setup_palette_fade3
+.import setup_palette_fade4
 .import apply_palette_fade_step
+.import apply_palette_fade_step2
+.import apply_palette_fade_step3
+.import apply_palette_fade_step4
 .import flush_palette
+.import flush_palette2
+.import flush_palette3
+.import flush_palette4
 
 .import target_palette
+.import target_palette2
+.import target_palette3
+.import target_palette4
 
 
 .macpack longbranch
@@ -86,25 +98,25 @@ clearloop:
 	stz Vera::Reg::Ctrl
 
 	MUSIC_SYNC $41
-	
+
 	; do wipe 1
 	lda #0
 	jsr wipe
 
 	MUSIC_SYNC $42
-	
+
 	; do wipe 2
 	lda #80
 	jsr wipe
 
 	MUSIC_SYNC $43
-	
+
 	; do wipe 3
 	lda #160
 	jsr wipe
 
 	MUSIC_SYNC $44
-	
+
 	; do wipe 4
 	lda #240
 	jsr wipe
@@ -122,20 +134,131 @@ clearloop:
 
 	PALETTE_FADE 1
 
+	MUSIC_SYNC $4A
+
+	jsr slide_off
+
+	LOADFILE "PANICPIC.VBM", 0, $0000, 0 ; $00000 VRAM
+	LOADFILE "PANICPIC.PAL", 0, target_palette
+
+	VERA_SET_ADDR Vera::VRAM_palette, 1
+	ldx #0
+panicpal_set:
+	lda target_palette,x
+	sta Vera::Reg::Data0
+	inx
+	bne panicpal_set
+
+	; set bitmap mode for layer 0
+	lda #%00000111 ; 8bpp
+	sta Vera::Reg::L0Config
+
+	MUSIC_SYNC $4C
+
+	jsr slide_on
+
 	MUSIC_SYNC $50
 
-	ldx #32
-:   stz target_palette-1,x
-	dex
+	ldx #0
+:   stz target_palette,x
+	inx
 	bne :-
 
 	lda #0
 	jsr setup_palette_fade
+	lda #64
+	jsr setup_palette_fade2
 
-	PALETTE_FADE 1
 
+	PALETTE_FADE_1_2 1
+
+	lda #(1 << 1) ; DCSEL=1
+	sta Vera::Reg::Ctrl
+	stz Vera::Reg::DCHStart
+	stz Vera::Reg::Ctrl
 
 	rts
+
+.proc slide_on
+	ldx #0
+slideloop:
+	phx
+	jsr apply_palette_fade_step
+	jsr apply_palette_fade_step2
+	WAITVSYNC
+	jsr flush_palette
+	jsr flush_palette2
+	plx
+	lda #(1 << 1) ; DCSEL=1
+	sta Vera::Reg::Ctrl
+	lda slide_on_hstart,x
+	sta Vera::Reg::DCHStart
+	stz Vera::Reg::Ctrl
+	inx
+	cpx #32
+	beq flash
+	cpx #48
+	bcc slideloop
+
+	rts
+flash:
+	VERA_SET_ADDR Vera::VRAM_palette, 1
+	ldy #128
+flashloop:
+	lda #$ff
+	sta Vera::Reg::Data0
+	lda #$0f
+	sta Vera::Reg::Data0
+	dey
+	bne flashloop
+
+	phx
+	lda #0
+	jsr setup_palette_fade
+	lda #64
+	jsr setup_palette_fade2
+	plx
+	bra slideloop
+
+slide_on_hstart:
+	.byte $a0,$9c,$98,$93
+	.byte $8f,$8b,$87,$82
+	.byte $7e,$7a,$76,$71
+	.byte $6d,$69,$65,$60
+	.byte $5c,$58,$54,$4f
+	.byte $4b,$47,$43,$3e
+	.byte $3a,$36,$32,$2d
+	.byte $29,$25,$21,$1c
+	.byte $18,$14,$12,$14
+	.byte $18,$1c,$1a,$18
+	.byte $16,$14,$12,$13
+	.byte $14,$15,$14,$14
+.endproc
+
+.proc slide_off
+	ldx #0
+slideloop:
+	phx
+	WAITVSYNC
+	plx
+	lda #(1 << 1) ; DCSEL=1
+	sta Vera::Reg::Ctrl
+	lda slide_off_hstart,x
+	sta Vera::Reg::DCHStart
+	stz Vera::Reg::Ctrl
+	inx
+	cpx #35
+	bcc slideloop
+
+	rts
+
+slide_off_hstart:
+	.byte 0,1,2,3,4,5,6,8
+	.byte 11,13,16,19,22,26,29,33
+	.byte 38,42,47,52,58,63,69,75
+	.byte 82,88,95,102,110,118,126,134
+	.byte 142,151,160
+.endproc
 
 
 .proc wipe
