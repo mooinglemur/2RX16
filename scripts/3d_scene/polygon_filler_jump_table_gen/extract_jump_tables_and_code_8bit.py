8520f05141a19c43170501bd3b158828b1efe8fb
Jeffrey <jeffrey@visual-trials.io>
2024-08-06 12:13:24 +0200

    extraction of tables and code

---
+++ scripts/3d_scene/polygon_filler_jump_table_gen/extract_jump_tables_and_code_8bit.py
@@ -0,0 +1,16 @@
+import os
+
+polyfull_8bit_dump_file = open('POLYFILL-8BIT-DUMP.BIN', 'rb')
+polyfull_8bit_dump_binary = polyfull_8bit_dump_file.read()
+polyfull_8bit_dump_file.close()
+
+# We are extracting from $3000 to $4A00
+from_pos = 0x3000
+to_pos = 0x4A00
+tables_and_code_bin = polyfull_8bit_dump_binary[from_pos:to_pos]
+
+print(len(tables_and_code_bin))
+print(tables_and_code_bin)
+
+with open("POLYFILL-8BIT-TBLS-AND-CODE.DAT", "wb") as tables_and_code_file:
+    tables_and_code_file.write(tables_and_code_bin)
