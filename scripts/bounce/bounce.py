5faa0ef8eba83debe342b85484586fa3fe4cbd01
Jeffrey <jeffrey@visual-trials.io>
2024-08-09 14:48:06 +0200

    bending action

--- scripts/bounce/bounce.py
+++ scripts/bounce/bounce.py
@@ -6,11 +6,13 @@ import math
 
 PRINT_MAP_AS_ASM = False  # otherwise write to DAT file
 PRINT_TILEDATA_AS_ASM = False  # otherwise write to DAT file
+SHOW_TILE_MAP = False
 
 source_image_filename = "assets/bounce/ICEKNGDM_320x200.png"
 source_image_width = 320
 source_image_height = 200
 source_left_padding = 70  # on the left of the source image there is a black 'padding' columns, we should exclude this
+
 
 tile_map_filename = "scripts/bounce/BOUNCE-TILEMAP.DAT"
 tile_pixel_data_filename = "scripts/bounce/BOUNCE-TILEDATA.DAT"
@@ -221,72 +223,72 @@ print("nr of unique tiles: " + str(len(unique_tiles.keys())))
     // Original calculation of y1 and y2 (per frame)
     
     // Scroll
-	a=64; y=400*64;
-	while(y>0)
-	{
-		y-=a;
-		a+=6;
-		if(y<0) y=0;
-		scrolly(y/64);
-		dis_waitb();
-	}
+a=64; y=400*64;
+while(y>0)
+{
+y-=a;
+a+=6;
+if(y<0) y=0;
+scrolly(y/64);
+dis_waitb();
+}
 
     // Bounce
-	frame=0;
-	ysz=400*16; ysza=-460/6;
-	y=0;
-	y1=0; y1a=500;
-	y2=399*16; y2a=500;
-	mika=1;
-	for(frame=0;frame<200;frame++)
-	{	
-		if(!halt)
-		{
-			y1+=y1a;
-			y2+=y2a;
-	
-			y2a+=16;
-			if(y2>400*16)
-			{
-				y2-=y2a;
-				y2a=-y2a*mika/8;
-				if(mika<4) mika+=3;
-			}
-	
-			y1a+=16;
-			
-			la=a;
-			a=(y2-y1)-400*16;
-			if((a&0x8000)^(la&0x8000))
-			{
-				y1a=y1a*7/8;
-			}
-			y1a+=a/8;
-			y2a-=a/8;
-		}
-		
-		if(frame>90) 
-		{
-			if(y2>=399*16) 
-			{
-				y2=400*16;
-				halt=1;
-			}
-			else y2a=8;
-			y1=y2-400*16;
-		}
+frame=0;
+ysz=400*16; ysza=-460/6;
+y=0;
+y1=0; y1a=500;
+y2=399*16; y2a=500;
+mika=1;
+for(frame=0;frame<200;frame++)
+{
+if(!halt)
+{
+y1+=y1a;
+y2+=y2a;
 
-		framey1[frame]=y1;
-		framey2[frame]=y2;
-	}
-	for(a=0;a<800;a++)
-	{
-		b=a/4;
-		c=a&3;
-		d=3-c;
-		framey1t[a]=(framey1[b]*d+framey1[b+1]*c)/3;
-		framey2t[a]=(framey2[b]*d+framey2[b+1]*c)/3;
-	}
+y2a+=16;
+if(y2>400*16)
+{
+y2-=y2a;
+y2a=-y2a*mika/8;
+if(mika<4) mika+=3;
+}
+
+y1a+=16;
+
+la=a;
+a=(y2-y1)-400*16;
+if((a&0x8000)^(la&0x8000))
+{
+y1a=y1a*7/8;
+}
+y1a+=a/8;
+y2a-=a/8;
+}
+
+if(frame>90) 
+{
+if(y2>=399*16) 
+{
+y2=400*16;
+halt=1;
+}
+else y2a=8;
+y1=y2-400*16;
+}
+
+framey1[frame]=y1;
+framey2[frame]=y2;
+}
+for(a=0;a<800;a++)
+{
+b=a/4;
+c=a&3;
+d=3-c;
+framey1t[a]=(framey1[b]*d+framey1[b+1]*c)/3;
+framey2t[a]=(framey2[b]*d+framey2[b+1]*c)/3;
+}
 
 '''
 
@@ -414,12 +416,16 @@ def generate_frames ():
     return total_frames
 
 total_frames = generate_frames()
-print(total_frames)
+#print(total_frames)
 
-
-screen_width = map_width*8
-screen_height = map_height*8
-scale = 3
+if (SHOW_TILE_MAP):
+    screen_width = map_width*8
+    screen_height = map_height*8
+    scale = 3
+else: 
+    screen_width = 320
+    screen_height = 240
+    scale = 2
 
 background_color = (0,0,0)
 
@@ -437,39 +443,44 @@ def run():
     
     screen.fill(background_color)
     
-    for tile_y in range(map_height):
-        for tile_x in range(map_width):
-        
-            sub_map_index = tile_y // 8
+    if (SHOW_TILE_MAP):
+        for tile_y in range(map_height):
+            for tile_x in range(map_width):
             
-            tile_index = tile_map[tile_y][tile_x]
-            tile_data = tiles_pixel_data[sub_map_index*192+tile_index]
-            
-            for y_in_tile in range(8):
-                for x_in_tile in range(8):
-                    clr_idx = tile_data[y_in_tile*8+x_in_tile]
-                    if clr_idx != 0:
-                        pixel_color = colors_12bit[tile_data[y_in_tile*8+x_in_tile]]
-                        
-                        x_screen = tile_x*8 + x_in_tile
-                        y_screen = tile_y*8 + y_in_tile
-                        
-                        pygame.draw.rect(screen, pixel_color, pygame.Rect(x_screen*scale, y_screen*scale, scale, scale))
+                sub_map_index = tile_y // 8
+                
+                tile_index = tile_map[tile_y][tile_x]
+                tile_data = tiles_pixel_data[sub_map_index*192+tile_index]
+                
+                for y_in_tile in range(8):
+                    for x_in_tile in range(8):
+                        clr_idx = tile_data[y_in_tile*8+x_in_tile]
+                        if clr_idx != 0:
+                            pixel_color = colors_12bit[tile_data[y_in_tile*8+x_in_tile]]
+                            
+                            x_screen = tile_x*8 + x_in_tile
+                            y_screen = tile_y*8 + y_in_tile
+                            
+                            pygame.draw.rect(screen, pixel_color, pygame.Rect(x_screen*scale, y_screen*scale, scale, scale))
     
+    frame_nr = 0
+    do_animate = False
     while running:
         # TODO: We might want to set this to max?
         clock.tick(60)
+
+        screen.fill(background_color)
         
         for event in pygame.event.get():
 
             if event.type == pygame.QUIT: 
                 running = False
 
-            '''
-            # if event.type == pygame.KEYDOWN:
+            if event.type == pygame.KEYDOWN:
                     
                 #if event.key == pygame.K_LEFT:
-                #if event.key == pygame.K_RIGHT:
+                if event.key == pygame.K_RIGHT:
+                    do_animate = True
                 #if event.key == pygame.K_COMMA:
                 #if event.key == pygame.K_PERIOD:
                 #if event.key == pygame.K_UP:
@@ -477,7 +488,54 @@ def run():
                     
             #if event.type == pygame.MOUSEMOTION: 
                 # newrect.center = event.pos
-            '''
+        
+        if (not do_animate):
+            continue
+            
+        frame_info = total_frames[frame_nr]
+        
+        if (frame_nr < len(total_frames)-1):
+            frame_nr += 1
+        
+        
+        pixel_color = (0xFF, 0xFF, 0x00)
+        
+        top_border = 20
+        left_border = 70
+        
+        y1 = frame_info['y1']/16
+        y2 = frame_info['y2']/16
+        
+        x_start = left_border
+        x_width = 184 # approx!
+        y_start = y1 / 2
+        y_end = y2 / 2
+        y_height = y_end - y_start
+        
+        xsc = (400-(y2-y1))/8
+            
+        # pygame.draw.rect(screen, pixel_color, pygame.Rect(x_start*scale, (y_start + top_border)*scale, x_width*scale, y_height*scale), 1*scale)
+        for y_in_picture in range(int(y_height)):
+            
+            y = y_start + y_in_picture
+            
+            # Only side borders:
+            #pygame.draw.rect(screen, pixel_color, pygame.Rect(x_start*scale, (y + top_border)*scale, scale, scale), 1*scale)
+            #pygame.draw.rect(screen, pixel_color, pygame.Rect((x_start+x_width)*scale, (y + top_border)*scale, scale, scale), 1*scale)
+            
+            # Filled:
+            
+            b = y_in_picture / y_height
+            # FIXME: +0.5?
+            a = (math.sin(b*math.pi)*xsc)
+            # FIXME: *even* number of pixels? a&=~1 ?
+
+            row_width = x_width + a*2
+            row_start = x_start - a
+            
+            pygame.draw.rect(screen, pixel_color, pygame.Rect(row_start*scale, (y + top_border)*scale, row_width*scale, scale), 1*scale)
+            
+            
         
         pygame.display.update()
         
